9520a9a761f724c99d2793eac3af91551e6dd704
Level.Z <1171567018@qq.com>
2018-09-28 21:42:19 +0800

    update

--- test/index.js
+++
@@ -1,8 +0,0 @@
-// const sinon = require('sinon');
-// const mongo = require('../src/database').mongo;
-// console.log('mongo123:', mongo);
-// sinon.stub(mongo, 'getDb').returns({ collection: () => {} });
-
-// module.exports = {
-//  controller: require('./controller')
-// };
